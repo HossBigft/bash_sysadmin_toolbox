11211be776b4d30be3146faaac0b00473a20a859
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-03-21 22:02:49 +0500

    Add fix to prevent silent exit when there is no email present

--- plesk_get_testmail_credentials_sudo.sh
+++ plesk_get_testmail_credentials_sudo.sh
@@ -20,7 +20,7 @@ get_mail_password() {
         printf "Error: Invalid input\n" 1>&2
         exit 1
     fi
-    /usr/local/psa/admin/bin/mail_auth_view | grep -F "${TEST_MAIL_LOGIN}@${domain}" | tr -d '[:space:]' | awk -F'|' '{print $4}'
+    /usr/local/psa/admin/bin/mail_auth_view | grep -F "${TEST_MAIL_LOGIN}@${domain}" | tr -d '[:space:]' | awk -F'|' '{print $4}' || true
 }
 
 create_testmail() {
