6962b35ecc134ff27d0c6a126d4c71dd005611b1
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-03-22 12:53:10 +0500

    Repalce regex with bash compatible

--- utils/validate_subscription_id.sh
+++ utils/validate_subscription_id.sh
@@ -5,7 +5,7 @@ set -o pipefail # don't hide errors within pipes
 
 validate_subscription_id() {
     local id="$1"
-    local int10_pattern='^(?:0|[1-9]\d{0,8}|42949672[0-8]\d|429496729[0-5])$' #plesk uses int10 internally
+    local int10_pattern='^(0|[1-9][0-9]{0,8}|42949672[0-8][0-9]|429496729[0-5])$' #plesk uses int10 internally
 
     if [[ ! "$id" =~ $int10_pattern ]]; then
         printf "Error: Invalid subscription id\n" >&2
