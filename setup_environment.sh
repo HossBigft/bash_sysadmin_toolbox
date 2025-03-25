fd7d4940a0986b5c8cbbe94af3a8da1ada480ec0
Maxim <m.glushchenko@office.hoster.kz>
2025-03-25 16:27:19 +0500

    Add script to setup db user

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,9 +5,11 @@ set -o pipefail # Don't hide errors within pipes
 
 source "$(dirname "${BASH_SOURCE[0]}")/utils/ensure_permissions.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/utils/sudo_rules_manager.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/utils/create_db_user.sh"
 main () {
     ensure_permissions
-    
+    append_sudo_rules_for_scripts
+    create_db_user
 }
 
 main
---
+++ utils/create_db_user.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+source "$(dirname "${BASH_SOURCE[0]}")/generate_password.sh"
+
+readonly DB_USER="plesk_utils"
+readonly DB_USER_ENV_VARIABLE="DATABASE_USER"
+readonly DB_PASS_ENV_VARIABLE="DATABASE_PASSWORD"
+create_db_user() {
+    local password
+    password="$(generate_password 15)"
+    local is_user_exists
+
+    is_user_exists="$(plesk db -Ne "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${DB_USER}')")"
+
+    if [[ is_user_exists -eq 0 ]]; then
+        plesk db "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${password}';"
+        #grant redonly access to all databases and tables
+        plesk db "GRANT SELECT on *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;"
+
+        echo "Writing database user to .env"
+        echo "${DB_USER_ENV_VARIABLE}=${DB_USER}" >>.env
+
+        echo "Writing database user password to .env"
+        echo "${DB_PASS_ENV_VARIABLE}=${password}" >>.env
+    else
+        printf "Database user %s already exists.\n" "$DB_USER"
+
+        if ! grep -q "$DB_USER_ENV_VARIABLE" ".env"; then
+            echo "Writing database user to .env"
+            echo "${DB_USER_ENV_VARIABLE}=${DB_USER}" >>.env
+        fi
+        exit
+    fi
+}
