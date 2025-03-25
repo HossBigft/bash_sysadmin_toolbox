795617c872305ffef53b1ffa8b642e95b57c482e
Maxim <m.glushchenko@office.hoster.kz>
2025-03-25 14:13:51 +0500

    Remove debugging

--- signed_executor.sh
+++ signed_executor.sh
@@ -7,7 +7,6 @@
 set -o errexit  # Abort on nonzero exit status
 set -o nounset  # Abort on unbound variable
 set -o pipefail # Don't hide errors within pipes
-set +x          # disable debugging
 
 
 source "$(dirname "${BASH_SOURCE[0]}")/utils/wrapper_manager.sh"
