9f202599865dc2d3d29032edf94acf5c0dbde8ae
Maxim <m.glushchenko@office.hoster.kz>
2025-03-27 17:37:59 +0500

    Make  public key unreadable for all users

--- update_public_key.sh
+++ update_public_key.sh
@@ -2,8 +2,9 @@
 set -o errexit  # Abort on nonzero exit status
 set -o nounset  # Abort on unbound variable
 set -o pipefail # Don't hide errors within pipes
-
+readonly KEY_NAME="public_key.pem"
 main() {
-    curl -s http://localhost:8000/api/v1/plesk/publickey >public_key.pem
+    curl -s http://localhost:8000/api/v1/plesk/publickey >"$KEY_NAME"
+    chmod 700 ./"$KEY_NAME"
 }
 main
